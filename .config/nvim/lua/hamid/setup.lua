29cb287be36156881085da93f49bcc6d615551d6
Hamid Sajjadi <hamid.sajjadi@volvocars.com>
2025-05-18 16:49:03 +0200

    update nvim config

--- .config/nvim/lua/hamid/setup.lua
+++ .config/nvim/lua/hamid/setup.lua
@@ -6,14 +6,28 @@ vim.opt.relativenumber = true     -- use relative line numbers
 
 
 -- set tab size to 2 spaces
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.smartindent = true
 
 vim.opt.wrap = false
 
+vim.opt.hlsearch = false
 vim.opt.incsearch = true -- incremental search
 
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/.undodir"
+vim.opt.undofile = true
+
 vim.opt.termguicolors = true
+
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+
+vim.opt.updatetime = 50
+
+vim.opt.colorcolumn = "80"
