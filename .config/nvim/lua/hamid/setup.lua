25ffe190db5c92e77e59b1c820db31b11d14e001
Hamid Sajjadi <hamid.sajjadi@volvocars.com>
2025-06-03 09:37:17 +0200

    nvim: remove column length line

--- .config/nvim/lua/hamid/setup.lua
+++ .config/nvim/lua/hamid/setup.lua
@@ -1,8 +1,8 @@
 vim.opt.clipboard = "unnamedplus" -- use system keyboard for yank
 
-vim.opt.nu = true -- set line numbers -- set line numbers
+vim.opt.nu = true                 -- set line numbers -- set line numbers
 
-vim.opt.relativenumber = true -- use relative line numbers
+vim.opt.relativenumber = true     -- use relative line numbers
 
 -- set tab size to 2 spaces
 vim.opt.tabstop = 4
@@ -28,7 +28,7 @@ vim.opt.signcolumn = "yes"
 
 vim.opt.updatetime = 50
 
-vim.opt.colorcolumn = "80"
+-- vim.opt.colorcolumn = "80"
 
 --require('telescope').load_extension('fzf')
 require("telescope").load_extension("fzf")
