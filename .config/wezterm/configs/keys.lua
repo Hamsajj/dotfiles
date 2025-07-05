df5f9b4ef774609130d7b620c1ae208f42bb6aec
Hamid Sajjadi <hamid.sajjadi@volvocars.com>
2025-07-06 00:05:42 +0200

    add split pane keys to wezTerm

--- .config/wezterm/configs/keys.lua
+++ .config/wezterm/configs/keys.lua
@@ -5,8 +5,23 @@ local M = {}
 function M.configure(config)
 	config.keys = {
 		{ key = "n", mods = "SHIFT|CTRL", action = wezterm.action.ToggleFullScreen },
-		{ key = "t", mods = "CTRL", action = wezterm.action.SpawnTab("DefaultDomain") },
 		{ key = "v", mods = "CTRL", action = wezterm.action({ PasteFrom = "Clipboard" }) },
+		{
+			key = "q",
+			mods = "SHIFT",
+			action = wezterm.action.SplitPane({
+				direction = "Down",
+				size = { Percent = 30 },
+			}),
+		},
+		{
+			key = "d",
+			mods = "SHIFT",
+			action = wezterm.action.SplitPane({
+				direction = "Right",
+				size = { Percent = 40 },
+			}),
+		},
 	}
 end
 
