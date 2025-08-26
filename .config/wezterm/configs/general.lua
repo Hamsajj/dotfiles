321322d03dff393a62666b37ebbed353e2b0bb04
Hamid Sajjadi <hamid.sajjadi@Hamids-MacBook-Pro.local>
2025-08-26 13:08:33 +0200

    wezterm: open in full screen

--- .config/wezterm/configs/general.lua
+++ .config/wezterm/configs/general.lua
@@ -4,6 +4,7 @@ function M.configure(config)
 	config.max_fps = 144
 	config.animation_fps = 60
 	config.cursor_blink_rate = 250
+
 end
 
 return M
--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,9 +1,15 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
+local mux = wezterm.mux
 
 if wezterm.target_triple == "x86_64-pc-windows-msvc" then
 	config.default_domain = "WSL:Ubuntu-22.04"
 end
+
+wezterm.on("gui-startup", function()
+	local _, _, window = mux.spawn_window({})
+	window:gui_window():maximize()
+end)
 
 require("configs").configure(config)
 
